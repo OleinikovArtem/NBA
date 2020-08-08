bfd57b378a439d6cc96ae75ece32e51306ac0e61
Wami Wamisoftware <wami@Wamis-MacBook-Pro.local>
2020-08-08 14:54:42 +0300

    Updates

---
+++ precache-manifest.78a9ffb1c8304981ff5e396c028f6b73.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7577bb963e5acbcb99a81c041906f6b9",
+    "url": "/nbi/index.html"
+  },
+  {
+    "revision": "b2ff5e7a91c1145d6a88",
+    "url": "/nbi/static/css/2.2f75dd9a.chunk.css"
+  },
+  {
+    "revision": "dae077a464cc3f4d5d7f",
+    "url": "/nbi/static/css/main.505b9e38.chunk.css"
+  },
+  {
+    "revision": "b2ff5e7a91c1145d6a88",
+    "url": "/nbi/static/js/2.f86969ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nbi/static/js/2.f86969ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dae077a464cc3f4d5d7f",
+    "url": "/nbi/static/js/main.58a39c42.chunk.js"
+  },
+  {
+    "revision": "08a1d1eae381f83a6bc8",
+    "url": "/nbi/static/js/runtime-main.5172f3d8.js"
+  },
+  {
+    "revision": "e6b68fc609f571f95336d1bb05b3a6f8",
+    "url": "/nbi/static/media/back.e6b68fc6.svg"
+  },
+  {
+    "revision": "5e35a88ee2cd7b3d3d86399af659f051",
+    "url": "/nbi/static/media/player.5e35a88e.png"
+  }
+]);
